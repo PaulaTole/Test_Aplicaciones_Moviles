f259c98cca6ac707831471ad2905bec34b4f71f8
Paulatole <pau.toledo@duocuc.cl>
2025-08-21 19:39:57 -0400

    Aver si sale

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Test_Apliaciones_Moviles',
+  webDir: 'www'
+};
+
+export default config;
