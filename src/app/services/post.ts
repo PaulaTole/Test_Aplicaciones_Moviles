7d679962bda305ed1a597e9d6ed14717152175ca
PaulaTole <pau.toledo@duocuc.cl>
2025-10-01 21:43:01 -0300

    avance

--- src/app/services/post.ts
+++ src/app/services/post.ts
@@ -1,8 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface post {
+
+  userid: number;
+  id:number ;
+  tiftle: string; 
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class Post {
+
+  private urlApi = "";
+
+  constructor(private Httpclient : HttpClient) {};
+
+  /*getPost():Observable.Post[]> {
+    return this.HttpClient.getPost[]>(this.urlApi);
+  }
+*/
   
 }
